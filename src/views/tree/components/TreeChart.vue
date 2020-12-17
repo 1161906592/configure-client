3a7b24dc22bcbda4ff459255ab46ea42830027cd
Yang.L <1161906592@qq.com>
2020-12-17 12:39:04 +0800

    tree

--- src/views/tree/components/TreeChart.vue
+++ src/views/tree/components/TreeChart.vue
@@ -4,28 +4,70 @@ export default {
   props: {
     data: { type: Object },
     renderContent: { type: Function },
-    direction: { type: String, default: "left" }
-    // space
+    direction: { type: String, default: "left" },
+    horizontalSpace: { type: Array, default: () => [20, 20] },
+    verticalSpace: { type: Array, default: () => [10, 20] },
+    lineOffset: { type: Number, default: 12 },
+    lineColor: { type: String, default: "#000" }
   },
 
   render(h) {
     const renderSlot = this.$scopedSlots.default;
-    const renderTree = data => {
+    const renderTree = (data, isFirst, isLast) => {
+      const len = data.children.length;
       const nodeChildren = [
         h(
           "div",
           {
-            class: "inner"
+            class: "content"
           },
-          this.renderContent ? this.renderContent(data) : renderSlot ? renderSlot(data) : null
+          [
+            h("div", {
+              style: {
+                position: "absolute",
+                height: "1px",
+                width: `${this.horizontalSpace[0]}px`,
+                left: 0,
+                top: isFirst ? `${this.lineOffset}px` : `${this.lineOffset + this.verticalSpace[1]}px`,
+                background: this.lineColor
+              }
+            }),
+            h(
+              "div",
+              {
+                class: "inner",
+                style: {
+                  padding: "8px 20px",
+                  border: "1px solid #eaeaea"
+                }
+              },
+              this.renderContent ? this.renderContent(data) : renderSlot ? renderSlot(data) : null
+            )
+          ]
         ),
-        data.children.length
+        len
           ? h(
               "div",
               {
-                class: "children"
+                class: "children",
+                style: {
+                  position: "relative",
+                  padding: `0 ${this.horizontalSpace[0]}px 0 ${this.horizontalSpace[1]}px`
+                }
               },
-              data.children.map(item => renderTree(item))
+              [
+                h("div", {
+                  style: {
+                    position: "absolute",
+                    height: "1px",
+                    width: `${this.horizontalSpace[1]}px`,
+                    left: "0",
+                    top: `${this.lineOffset}px`,
+                    background: this.lineColor
+                  }
+                }),
+                ...data.children.map((item, index) => renderTree(item, index === 0, index === len - 1))
+              ]
             )
           : null
       ];
@@ -37,9 +79,40 @@ export default {
       return h(
         "div",
         {
-          class: "node"
+          class: "node",
+          style: {
+            display: "flex",
+            position: "relative",
+            padding: `${this.verticalSpace[1]}px ${this.horizontalSpace[1]}px ${this.verticalSpace[0]}px ${this.horizontalSpace[0]}px`,
+            paddingTop: isFirst ? "0px" : undefined,
+            paddingBottom: isLast ? "0px" : undefined
+          }
         },
-        nodeChildren
+        [
+          !isFirst &&
+            h("div", {
+              style: {
+                position: "absolute",
+                width: "1px",
+                height: `${this.lineOffset + this.verticalSpace[1]}px`,
+                left: 0,
+                top: 0,
+                background: this.lineColor
+              }
+            }),
+          ...nodeChildren,
+          !isLast &&
+            h("div", {
+              style: {
+                position: "absolute",
+                width: "1px",
+                height: `calc(100% - ${this.lineOffset}px)`,
+                left: 0,
+                top: `${this.lineOffset}px`,
+                background: this.lineColor
+              }
+            })
+        ].filter(Boolean)
       );
     };
     return h(
@@ -52,192 +125,3 @@ export default {
   }
 };
 </script>
-
-<style scoped lang="scss">
-.node {
-  position: relative;
-  display: flex;
-  align-items: center;
-  &:before,
-  &:after {
-    content: "";
-    position: absolute;
-  }
-  &:first-child:before,
-  &:last-child:after {
-    content: none;
-  }
-}
-.inner {
-  padding: 8px 20px;
-  border: 1px solid #eaeaea;
-  &:before {
-    content: "";
-    position: absolute;
-  }
-}
-.children {
-  position: relative;
-  &:before {
-    content: "";
-    position: absolute;
-  }
-}
-.root {
-  &:before {
-    content: none;
-  }
-}
-
-.left,
-.right {
-  .node {
-    padding: 4px 20px;
-    &:before,
-    &:after {
-      height: 50%;
-      width: 19px;
-    }
-    &:before {
-      top: 0;
-    }
-    &:after {
-      top: 50%;
-    }
-    &:first-child {
-      padding-top: 0;
-    }
-    &:last-child {
-      padding-bottom: 0;
-    }
-  }
-  .inner {
-    &:before {
-      height: 1px;
-      width: 19px;
-      top: 50%;
-      border-top: 1px solid #eaeaea;
-    }
-  }
-  .children {
-    padding: 0 20px;
-    &:before {
-      height: 1px;
-      width: 20px;
-      top: 50%;
-      border-top: 1px solid #eaeaea;
-    }
-  }
-}
-
-.left {
-  .node {
-    &:before,
-    &:after {
-      left: 0;
-      border-left: 1px solid #eaeaea;
-    }
-  }
-  .inner,
-  .children {
-    &:before {
-      left: 0;
-    }
-  }
-}
-
-.right {
-  .node {
-    justify-content: flex-end;
-
-    &:after,
-    &:before {
-      right: 0;
-      border-right: 1px solid #eaeaea;
-    }
-  }
-  .inner,
-  .children {
-    &:before {
-      right: 0;
-    }
-  }
-}
-
-.top,
-.bottom {
-  .node {
-    flex-direction: column;
-    padding: 20px 4px;
-    &:before,
-    &:after {
-      width: 50%;
-      height: 19px;
-    }
-    &:before {
-      left: 0;
-    }
-    &:after {
-      left: 50%;
-    }
-    &:first-child {
-      padding-left: 0;
-    }
-    &:last-child {
-      padding-right: 0;
-    }
-  }
-  .inner {
-    &:before {
-      width: 1px;
-      height: 19px;
-      left: 50%;
-      border-left: 1px solid #eaeaea;
-    }
-  }
-  .children {
-    display: flex;
-    padding: 20px 0;
-    &:before {
-      width: 1px;
-      height: 20px;
-      left: 50%;
-      border-left: 1px solid #eaeaea;
-    }
-  }
-}
-
-.top {
-  .node {
-    &:before,
-    &:after {
-      top: 0;
-      border-top: 1px solid #eaeaea;
-    }
-  }
-  .inner,
-  .children {
-    &:before {
-      top: 0;
-    }
-  }
-}
-
-.bottom {
-  .node {
-    justify-content: flex-end;
-
-    &:after,
-    &:before {
-      bottom: 0;
-      border-bottom: 1px solid #eaeaea;
-    }
-  }
-  .inner,
-  .children {
-    &:before {
-      bottom: 0;
-    }
-  }
-}
-</style>
